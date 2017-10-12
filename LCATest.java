56e36a21dfef0cc3e1679a2f0b67f9bc41ec936d
Yitpin Chin <chiny@tcd.ie>
2017-10-12 17:46:35 +0100

    Test Case Ideas

--- LCATest.java
+++ LCATest.java
@@ -5,8 +5,33 @@ import org.junit.Test;
 public class LCATest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testNodeConstructor(){
+		
+		
 	}
-
+	
+	@Test
+	public void testEmptyTree() {
+		
+		
+	}
+	
+	@Test
+	public void testCommonAncestor(){
+		
+		
+	}
+	
+	@Test
+	public void testTwoNodes(){
+		
+		
+	}
+	
+	@Test
+	public void testForNonExistentNodes(){
+	
+		
+	}
+	
 }
