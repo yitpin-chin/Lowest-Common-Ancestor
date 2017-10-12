123eb15ce0af83aa82d02d5fb429838130ef86f1
Yitpin Chin <chiny@tcd.ie>
2017-10-12 18:18:31 +0100

    Test for non-existent nodes

--- LCATest.java
+++ LCATest.java
@@ -51,7 +51,21 @@ public class LCATest {
 	@Test
 	public void testForNonExistentNodes(){
 	
+		//test tree structure:
+		//		1
+		//	2		3
+		//4   5   6   7
+		LCA tree = new LCA();
+		tree.root = new Node(1);
+		tree.root.left = new Node(2);
+		tree.root.right = new Node(3);
+		tree.root.left.left = new Node(4);
+		tree.root.left.right = new Node(5);
+		tree.root.right.left = new Node(6);
+		tree.root.right.right = new Node(7);
 		
+		assertEquals("Find ancestors of non-existent nodes in populated tree: ", null, tree.findLCA(8, 9));
+		assertEquals("Find ancestors of non-existent nodes in populated tree: ", null, tree.findLCA(45, 93));
 	}
 	
 }
