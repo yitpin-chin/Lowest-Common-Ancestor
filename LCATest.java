91f18eb44ba3cb8c16417d512c662754089d6a0c
Yitpin Chin <chiny@tcd.ie>
2017-10-12 18:09:15 +0100

    Test Common Ancestor
    with populated tree

--- LCATest.java
+++ LCATest.java
@@ -14,13 +14,32 @@ public class LCATest {
 	@Test
 	public void testEmptyTree() {
 		
-		
+		LCA tree = new LCA();
+		assertNull("Find LCA when tree is empty: ", tree.findLCA(1, 2));
+		assertEquals("Find LCA when tree is empty: ", null, tree.findLCA(1, 2) );
 	}
 	
 	@Test
 	public void testCommonAncestor(){
 		
-		
+		//test tree structure:
+		//		1
+		//	2		3
+		//4   5   6   7
+		LCA tree = new LCA();
+		tree.root = new Node(1);
+		tree.root.left = new Node(2);
+		tree.root.right = new Node(3);
+		tree.root.left.left = new Node(4);
+		tree.root.left.right = new Node(5);
+		tree.root.right.left = new Node(6);
+		tree.root.right.right = new Node(7);
+
+		assertEquals("LCA of 4 and 5: ", 2, tree.findLCA(4, 5).data);
+		assertEquals("LCA of 6 and 7: ", 3, tree.findLCA(6, 7).data);
+		assertEquals("LCA of 2 and 3: ", 1, tree.findLCA(2, 3).data);
+		assertEquals("LCA of 4 and 7: ", 1, tree.findLCA(4, 7).data);
+
 	}
 	
 	@Test
