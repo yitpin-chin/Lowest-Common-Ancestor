c10113fa13d342c487e0c0716a1c77223d67e6f4
Yitpin Chin <chiny@tcd.ie>
2017-10-13 11:11:57 +0100

    Solution
    tested against test codes and passing the test cases.

--- LCA.java
+++ LCA.java
@@ -11,6 +11,38 @@ class Node {
 }
 
 public class LCA {
+	
+	Node root;
+
+	Node findLCA(int n1, int n2) {
+
+		return findLCA(root, n1, n2);
+	}
+
+	Node findLCA(Node node, int n1, int n2) {
+
+		if (node == null) {
+			return null;
+		}
+
+		if (node.data == n1 || node.data == n2) {
+			return node;
+		}
+
+		Node left = findLCA(node.left, n1, n2);
+		Node right = findLCA(node.right, n1, n2);
+
+		if (left != null && right != null) {
+			return node;
+		}
+
+		if (left != null) {
+			return left;
+		} else {
+			return right;
+		}
+
+	}
 
 	
 }
