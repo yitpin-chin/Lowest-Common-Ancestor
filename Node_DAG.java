e5f4150cade00035d5d9d79dfc86c492e65ea8bc
Yitpin Chin <chiny@tcd.ie>
2017-10-31 16:09:26 +0000

    Update node class

--- Node_DAG.java
+++ Node_DAG.java
@@ -1,15 +1,19 @@
-import java.util.Set;
+import java.util.*;
 
 public class Node_DAG<T> {
 
 	Set<Node_DAG<T>> point;
 	T data;
 	
-	
 	public Node_DAG(T data){
 		
+		this.data = data;
+		this.point = new HashSet<>();
 	}
 	
-	
+	public void addEdge(Node_DAG<T> edge){
+		
+		point.add(edge);
+	}
 	
 }
