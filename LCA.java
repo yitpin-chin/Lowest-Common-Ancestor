a9fe5bfab03ff6de33fe69c70f34cd22843daa71
Yitpin Chin <chiny@tcd.ie>
2017-10-31 17:03:13 +0000

    DFS

--- LCA.java
+++ LCA.java
@@ -1,7 +1,27 @@
+import java.util.*;
 
-  public class LCA {
+  public class LCA<T> {
 	
-
+	  Node_DAG root;
+	  
+	  List<Node_DAG<T>> DFS(List<Node_DAG<T>> list, Node_DAG<T> node, Node_DAG<T> searchNode, Stack<Node_DAG<T>> stack){
+		  
+		  stack.push(node);
+		  
+		  for(Node_DAG<T> n : node.point){
+			  
+			  if(n.equals(searchNode)){
+				  
+				  list.addAll(stack);
+				  return list;
+			  }
+			  
+			  DFS(list, node, searchNode, stack);
+		  }
+		  
+		  stack.pop();
+		  return list;
+	  }
 
 	
 }
