716374f03399392a6919fc1570bc4d89a37f3051
Yitpin Chin <chiny@tcd.ie>
2017-10-31 17:34:36 +0000

    added some list

--- LCA.java
+++ LCA.java
@@ -4,7 +4,7 @@ import java.util.*;
 	
 	  Node_DAG root;
 	  
-	  List<Node_DAG<T>> DFS(List<Node_DAG<T>> list, Node_DAG<T> node, Node_DAG<T> searchNode, Stack<Node_DAG<T>> stack){
+	  List<Node_DAG<T>> DFS(List<Node_DAG<T>> list, Stack<Node_DAG<T>> stack, Node_DAG<T> node, Node_DAG<T> searchNode){
 		  
 		  stack.push(node);
 		  
@@ -16,11 +16,30 @@ import java.util.*;
 				  return list;
 			  }
 			  
-			  DFS(list, node, searchNode, stack);
+			  DFS(list, stack, node, searchNode);
 		  }
 		  
 		  stack.pop();
 		  return list;
+	  }
+	  
+	  public Node_DAG<T> LCA(Node_DAG<T> root, Node_DAG<T> n1, Node_DAG<T> n2){
+		  
+		  List<Node_DAG<T>> list1 = DFS(new ArrayList<>(), new Stack<>(), root, n1);
+		  List<Node_DAG<T>> list2 = DFS(new ArrayList<>(), new Stack<>(), root, n2);
+		  List<Node_DAG<T>> minList;
+		  List<Node_DAG<T>> maxList;
+		  
+		  if(root == null || n1 == null || n2 == null){
+			  
+			  return null;
+		  }
+		  
+		  if(list1 == null || list2 == null){
+			  
+			  return null;
+		  }
+		  
 	  }
 
 	
